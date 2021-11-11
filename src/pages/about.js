682a7e01620543e1de4a2b0f9be8ce52f329085a
Dee Fialho <deefialho@gmail.com>
2021-11-10 23:15:14 -0300

    gatsby test

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import Layout from '../components/layout'
+import React from 'react'
+
+const about = () => {
+    return (
+        <Layout pageTitle="About Me">
+        <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      </Layout>
+    )
+}
+
+export default about
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+// Step 1: Import React
+import Layout from '../components/layout'
+import * as React from 'react'
+
+// Step 2: Define your component
+const IndexPage = () => {
+  return (
+   <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
+   
+  )
+}
+
+// Step 3: Export your component
+export default IndexPage
